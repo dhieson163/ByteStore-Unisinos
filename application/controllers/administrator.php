79b5537e42de1be552f25b24542e31b690793b7c
dhieson163 <dhieson_arenhart@hotmail.com>
2011-09-03 14:53:46 -0300

    Teste

--- application/controllers/administrator.php
+++ application/controllers/administrator.php
@@ -9,6 +9,7 @@ class administrator extends CI_Controller {
         $this->load->view('partials/admin/header',$data);
         $this->load->view('admin/home/index');
         $this->load->view('partials/admin/footer');
+        
     }
 }
 ?>
